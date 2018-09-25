dd29d43b43d50dad55d7081a531322f1106cc4c7
PollyannaCatao <polly_catao@hotmail.com>
2018-09-25 13:47:34 -0300

    5

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
 import {RadioOption} from '../shared/radio/radio-option.model'
 import {OrderService} from './order.service';
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
 import {Order, OrderItem} from './order.model'
-import {Router} from '@angular/router';
+
 
 
 @Component({
@@ -83,8 +84,7 @@ export class OrderComponent implements OnInit {
     .map((item:CartItem)=>new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order)
     .subscribe( (orderId: string) => {
-      this.router.navigate(['/order-summary'])
-      console.log(`Compra Concluida: ${orderId}`)
+      this.router.navigate(['/order-summary'])      
       this.orderService.clear()
     })
     console.log(order)
