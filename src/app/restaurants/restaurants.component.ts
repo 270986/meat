57f488d44ce041598be6bc0a1e7b084c752352b3
PollyannaCatao <polly_catao@hotmail.com>
2018-10-10 17:00:53 -0300

    101118

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -4,7 +4,14 @@ import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Restaurant} from './restaurant/restaurant.model';
 import {RestaurantsService} from './restaurants.service';
 
-import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
+import {Observable} from 'rxjs/Observable';
+
 
 @Component({
   selector: 'mt-restaurants',
@@ -44,8 +51,12 @@ export class RestaurantsComponent implements OnInit {
     })
 
     this.searchControl.valueChanges
+        .debounceTime(500)
+        .distinctUntilChanged()
         .switchMap(searchTerm => 
-          this.restaurantsService.restaurants(searchTerm))
+          this.restaurantsService
+          .restaurants(searchTerm)
+          .catch(error=>Observable.from([])))
         .subscribe(restaurants => this.restaurants = restaurants)
     
 
